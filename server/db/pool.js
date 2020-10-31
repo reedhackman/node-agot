31a37fe56d8aa6259fe8365e1e34f14757028930
Michael Hackman <reedhackman@gmail.com>
2020-10-30 17:10:06 -0700

    stuff

---
+++ server/db/pool.js
@@ -0,0 +1,11 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: process.env.RDS_USERNAME,
+  host: process.env.RDS_HOSTNAME,
+  database: process.env.RDS_DB_NAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT,
+});
+
+module.exports = pool;
